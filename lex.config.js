72eba5885502ff79e6ad9b756d1b16cb1fa6d504
treethought <cpsweene@gmail.com>
2026-02-08 12:48:23 -0800

    Revert "remove lex generated code (#21)" (#23)
    This reverts commit 1418f94d77704dfae4594a680804fa400e87d987.

---
+++ lex.config.js
@@ -0,0 +1,25 @@
+// file: lex.config.js
+import { defineLexiconConfig } from '@atcute/lex-cli';
+
+export default defineLexiconConfig({
+	files: ['lexicons/**/*.json'],
+	outdir: 'src/lexicons/',
+	pull: {
+		outdir: 'lexicons/',
+		clean: false,
+		sources: [
+			{
+				type: 'git',
+				remote: 'https://github.com/cosmik-network/semble.git',
+				ref: 'main',
+				pattern: ['src/modules/atproto/infrastructure/lexicons/**/*.json'],
+			},
+			{
+				type: 'git',
+				remote: 'https://tangled.org/margin.at/margin.git',
+				ref: 'main',
+				pattern: ['lexicons/**/*.json'],
+			},
+		],
+	},
+});
